c73724c12d4df4c8ae9450c34a105515823da286
Denis <den90021@gmail.com>
2024-04-09 21:09:55 +0300

    added firebase connection and landing screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:redox_ui/colors.dart';
-import 'package:redox_ui/screens/mobile_layout_screen.dart';
+import 'package:redox_ui/features/landing/screens/landing_screen.dart';
+import 'package:redox_ui/firebase_options.dart';
+/*import 'package:redox_ui/screens/mobile_layout_screen.dart';
 import 'package:redox_ui/screens/web_layout_screen.dart';
-import 'package:redox_ui/utils/responsive_layout.dart';
+import 'package:redox_ui/utils/responsive_layout.dart';*/
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const  MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,16 +19,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( 
       debugShowCheckedModeBanner: false,
       title: 'redox UI',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
       ),
-      home: const ResponsiveLayout(
+      home: const LandingScreen(),
+      /*const ResponsiveLayout(
         mobileScreenLayout: MobileLayoutScreen(),
         webScreenLayout: WebLayoutScreen(),
-      ),
+      ),*/
     );
   }
 }
